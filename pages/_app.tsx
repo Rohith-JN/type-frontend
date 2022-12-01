2f1f3116d315854355f77adb811c1386548bea74
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-12-01 20:20:28 +0530

    Added styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import '../styles/globals.css'
+import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
+import Router from "next/router";
+import { useState, useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
+  return <Layout><Component {...pageProps} /></Layout>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#262a33',
+        'secondary': '#43ffaf',
+        'caret': '#43ffaf',
+        'sub': '#526777',
+        'sub-alt': '#1f232c',
+        'tertiary': '#e5f7ef',
+        'error': '#ff5f5f',
+        'error-extra': '#d22a2a'
+      },
+      fontFamily: {
+        lexend: ["LEXEND"],
+      },
+    },
   },
   plugins: [],
 }
