c32ce5531571618be3fee83d284f6361556f1ee9
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-12-09 15:32:24 +0530

    Removed Tailwind

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,15 @@
-const Navbar = () => {
+import styles from '../styles/Navbar.module.css';
 
+const Navbar = () => {
   return (
-    <div className="w-full flex justify-center items-center fixed top-4">
-      <div className="w-3/5 h-10 rounded flex flex-row">
-        <div className="flex-row flex justify-between w-full items-center h-16">
-          <h1 className="text-5xl text-tertiary font-lexend">Type</h1>
-          <div className="w-2/5 h-12 bg-sub-alt rounded-xl mt-2 flex flex-row justify-around items-center">
-            <h1 className="text-sub text-lg hover:text-white cursor-pointer font-lexend">type</h1>
-            <h1 className="text-sub text-lg hover:text-white cursor-pointer font-lexend">account</h1>  
-            <h1 className="text-sub text-lg hover:text-white cursor-pointer font-lexend">leaderboard</h1>
+    <div className={styles.Navbar}>
+      <div className={styles.Nav_Container}>
+        <div className={styles.Nav}>
+          <h1 className={styles.heading}>Type</h1>
+          <div className={styles.Nav_Items}>
+            <h1 className={styles.Nav_Text}>type</h1>
+            <h1 className={styles.Nav_Text}>account</h1>  
+            <h1 className={styles.Nav_Text}>leaderboard</h1>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
+import styles from '../styles/Home.module.css';
+
 export default function Home() {
   return (
     <>
-      <div className='w-full flex flex-col items-center bg-primary h-screen'>
-        <div className='w-1/2 h-10 rounded-xl mt-28 flex flex-row bg-sub-alt justify-around items-center'>
-          <h1 className="text-sub text-md hover:text-white cursor-pointer font-lexend">@punctuation</h1>
-          <h1 className="text-sub text-md hover:text-white cursor-pointer font-lexend">#numbers</h1>
-          <div className="h-5 w-1 bg-primary rounded-md"></div>
-          <h1 className="text-sub text-md hover:text-white cursor-pointer font-lexend">time</h1>
-          <h1 className="text-sub text-md hover:text-white cursor-pointer font-lexend">quote</h1>
-          <h1 className="text-sub text-md hover:text-white cursor-pointer font-lexend">words</h1>
-          <div className="h-5 w-1 bg-primary rounded-md"></div>
-          <h1 className="text-sub text-md hover:text-white cursor-pointer font-lexend">15</h1>
-          <h1 className="text-sub text-md hover:text-white cursor-pointer font-lexend">30</h1>
-          <h1 className="text-sub text-md hover:text-white cursor-pointer font-lexend">60</h1>
-          <h1 className="text-sub text-md hover:text-white cursor-pointer font-lexend">120</h1>
+      <div className={styles.Nav_Container}>
+        <div className={styles.NavBar}>
+          <h1 className={styles.Nav_Text}>@punctuation</h1>
+          <h1 className={styles.Nav_Text}>#numbers</h1>
+          <div className={styles.Divider}></div>
+          <h1 className={styles.Nav_Text}>time</h1>
+          <h1 className={styles.Nav_Text}>quote</h1>
+          <h1 className={styles.Nav_Text}>words</h1>
+          <div className={styles.Divider}></div>
+          <h1 className={styles.Nav_Text}>15</h1>
+          <h1 className={styles.Nav_Text}>30</h1>
+          <h1 className={styles.Nav_Text}>60</h1>
+          <h1 className={styles.Nav_Text}>120</h1>
         </div>
+        <div className={styles.Input_Container}></div>
       </div>
     </>
   )
