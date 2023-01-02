2270df70b6afb98687f50d30f759ca569b5e77bf
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-01-02 15:53:21 +0530

    Updated design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,27 +6,27 @@ import { useState, useEffect } from 'react';
 import Loader from '../components/Loader';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const start = () => {
-      setLoading(true);
-    };
-    const end = () => {
-      setLoading(false);
-    };
-    Router.events.on("routeChangeStart", start);
-    Router.events.on("routeChangeComplete", end);
-    Router.events.on("routeChangeError", end);
-    return () => {
-      Router.events.off("routeChangeStart", start);
-      Router.events.off("routeChangeComplete", end);
-      Router.events.off("routeChangeError", end);
-    };
-  }, []);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        const start = () => {
+            setLoading(true);
+        };
+        const end = () => {
+            setLoading(false);
+        };
+        Router.events.on("routeChangeStart", start);
+        Router.events.on("routeChangeComplete", end);
+        Router.events.on("routeChangeError", end);
+        return () => {
+            Router.events.off("routeChangeStart", start);
+            Router.events.off("routeChangeComplete", end);
+            Router.events.off("routeChangeError", end);
+        };
+    }, []);
 
-  return (
-      <Layout>
-        {loading ? (<Loader />) : (<Component {...pageProps} />)}
-      </Layout>
-  );
+    return (
+        <Layout>
+            {loading ? (<Loader />) : (<Component {...pageProps} />)}
+        </Layout>
+    );
 }
