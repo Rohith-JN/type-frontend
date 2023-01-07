d76f89ca95fc5e183ac04dc3a09380756610b440
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-01-07 19:30:14 +0530

    Modified navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,48 @@
 import styles from '../styles/Home.module.css';
 import { useState } from 'react'
-export default function Home() {
-   const time = ["15", "30", "60", "120"]
-   const words = ["10", "25", "50", "100"]
-   const quote = []
-   const list:string[] = ["10"]
+
+function NavOption(props) {
+    const { optionText, isSelected, onClick } = props;
+
+    return (
+        <div onClick={onClick}>
+            {isSelected ? <h1 className={styles.Nav_Text} style={{ color: 'var(--main-color)' }}>{optionText}</h1> : <h1 className={styles.Nav_Text}>{optionText}</h1>}
+        </div >
+    );
+}
+
+export default function Home(props) {
+    const [punctuation, setPunctuation] = useState(false);
+    const [numbers, setNumbers] = useState(false);
+    const [advanced, setAdvanced] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(2);
+    const options = [
+        { id: 1, optionText: '15' },
+        { id: 2, optionText: '30' },
+        { id: 3, optionText: '60' },
+        { id: 4, optionText: '120' },
+        { id: 5, optionText: '300' },
+    ];
+
     return (
         <>
             <div className={styles.Nav_Container}>
                 <div className={styles.NavBar}>
-                    <h1 className={styles.Nav_Text}>@punctuation</h1>
-                    <h1 className={styles.Nav_Text}>#numbers</h1>
+                    <h1 className={styles.Nav_Text} onClick={() => setPunctuation(!punctuation)} style={punctuation ? { color: 'var(--main-color)' } : { color: 'var(--sub-color)' }}>@ punctuation</h1>
+                    <h1 className={styles.Nav_Text} onClick={() => setAdvanced(!advanced)} style={advanced ? { color: 'var(--main-color)' } : { color: 'var(--sub-color)' }}>$ advanced</h1>
+                    <h1 className={styles.Nav_Text} onClick={() => setNumbers(!numbers)} style={numbers ? { color: 'var(--main-color)' } : { color: 'var(--sub-color)' }}># numbers</h1>
                     <div className={styles.Divider}></div>
-                    <h1 className={styles.Nav_Text}>time</h1>
-                    <h1 className={styles.Nav_Text}>quote</h1>
-                    <h1 className={styles.Nav_Text}>words</h1>
-                    <div className={styles.Divider}></div>
-                    {words.map((element: string) => <div className={styles.Nav_Text} key = {element}></div>)}
+                    {options.map((option) => (
+                        <NavOption
+                            key={option.id}
+                            optionText={option.optionText}
+                            isSelected={option.id === selectedOption}
+                            onClick={() => setSelectedOption(option.id)}
+                        />
+                    ))}
                 </div>
-                <div className={styles.Input_Container}></div>
+                <div className={styles.Input_Container}>
+                </div>
             </div>
         </>
     )
