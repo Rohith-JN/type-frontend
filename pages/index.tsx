0bc61f0b584b476f8d35160e3e716020bd5005e7
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-02-15 22:06:22 +0530

    Daily commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import styles from '../styles/Home.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function NavOption(props: { optionText: string, isSelected: boolean, onClick: () => void }) {
     const { optionText, isSelected, onClick } = props;
 
     return (
         <div onClick={onClick}>
-            {isSelected ? <h1 className={styles.Nav_Text} style={{ color: 'var(--main-color)' }}>{optionText}</h1> : <h1 className={styles.Nav_Text}>{optionText}</h1>}
+            {isSelected ? <h1 className={styles.NavText} style={{ color: 'var(--main-color)' }}>{optionText}</h1> : <h1 className={styles.NavText}>{optionText}</h1>}
         </div >
     );
 }
@@ -16,6 +16,8 @@ export default function Home() {
     const [numbers, setNumbers] = useState(false);
     const [advanced, setAdvanced] = useState(false);
     const [selectedOption, setSelectedOption] = useState(2);
+    const inputRef = useRef<HTMLInputElement>(null);
+
     const options = [
         { id: 1, optionText: '15' },
         { id: 2, optionText: '30' },
@@ -26,11 +28,11 @@ export default function Home() {
 
     return (
         <>
-            <div className={styles.Nav_Container}>
+            <div className={styles.Container}>
                 <div className={styles.NavBar}>
-                    <h1 className={styles.Nav_Text} onClick={() => setPunctuation(!punctuation)} style={punctuation ? { color: 'var(--main-color)' } : { color: 'var(--sub-color)' }}>@ punctuation</h1>
-                    <h1 className={styles.Nav_Text} onClick={() => setAdvanced(!advanced)} style={advanced ? { color: 'var(--main-color)' } : { color: 'var(--sub-color)' }}>$ advanced</h1>
-                    <h1 className={styles.Nav_Text} onClick={() => setNumbers(!numbers)} style={numbers ? { color: 'var(--main-color)' } : { color: 'var(--sub-color)' }}># numbers</h1>
+                    <h1 className={styles.NavText} onClick={() => setPunctuation(!punctuation)} style={punctuation ? { color: 'var(--main-color)' } : { color: 'var(--sub-color)' }}>@ punctuation</h1>
+                    <h1 className={styles.NavText} onClick={() => setAdvanced(!advanced)} style={advanced ? { color: 'var(--main-color)' } : { color: 'var(--sub-color)' }}>$ advanced</h1>
+                    <h1 className={styles.NavText} onClick={() => setNumbers(!numbers)} style={numbers ? { color: 'var(--main-color)' } : { color: 'var(--sub-color)' }}># numbers</h1>
                     <div className={styles.Divider}></div>
                     {options.map((option) => (
                         <NavOption
@@ -41,7 +43,12 @@ export default function Home() {
                         />
                     ))}
                 </div>
-                <div className={styles.Input_Container}>
+                <div className={styles.TextContainer}></div>
+                <div className={styles.InputContainer}>
+                    <input type="search" ref={inputRef} autoComplete="off" spellCheck='false'></input>
+                    <div className={styles.WPM}></div>
+                    <div className={styles.Time}></div>
+                    <div className={styles.Reset}></div>
                 </div>
             </div>
         </>
