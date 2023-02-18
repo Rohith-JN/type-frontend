65334150311bc0cae3c348391f81ae7961dc22da
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-02-18 18:08:33 +0530

    Added firebase

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
 import styles from '../styles/Signup.module.css';
 import { useRouter } from 'next/router'
+import React, { useState } from 'react';
 import { NextPage } from 'next';
 
 const Signup: NextPage = () => {
     const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
     return (
         <>
             <div className={styles.Signup}>
                 <h1>Sign Up</h1>
                 <form role='form'>
                     <input autoComplete="off" spellCheck='false' type="text" placeholder='Name'></input>
-                    <input autoComplete="off" spellCheck='false' type="email" placeholder='Email' ></input>
-                    <input autoComplete="off" spellCheck='false' type="password" placeholder='Password'></input>
+                    <input autoComplete="off" spellCheck='false' type="email" placeholder='Email' value={email}
+                        onChange={(event) => setEmail(event.target.value)}></input>
+                    <input autoComplete="off" spellCheck='false' type="password" placeholder='Password' value={password}
+                        onChange={(event) => setPassword(event.target.value)}></input>
                     <button type="submit" value="Submit" className={styles.slide}>Sign up</button>
                 </form>
                 <p>Already have an account? <span onClick={() => router.push("/login")}>Log In</span></p>
