2e51af661aaf82c6d66fa082939b0d67421d0080
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-02-24 14:51:33 +0530

    Minor bug fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,6 +36,8 @@ export const Header = () => {
         import(`../data/english.json`).then((words) =>
             dispatch(setWordList(words))
         );
+        dispatch(timerSet(option));
+        dispatch(setTime(option));
         setSelectedOption(timeOptions.find(opt => opt.optionText === option)?.id || 2);
     }, [dispatch, option, timeOptions]);
 
