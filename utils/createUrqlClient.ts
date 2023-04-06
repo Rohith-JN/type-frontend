bfb1b773204989aeef8a8ff2ea707dd504c2d49d
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-04-06 20:26:39 +0530

    Implemented CursorPagination

--- utils/createUrqlClient.ts
+++ utils/createUrqlClient.ts
@@ -1,6 +1,59 @@
-import { dedupExchange, fetchExchange } from "urql";
+import { Resolver, cacheExchange,  } from "@urql/exchange-graphcache";
+import { dedupExchange, fetchExchange, stringifyVariables } from "urql";
+
+const cursorPagination = (): Resolver => {
+    return (_parent, fieldArgs, cache, info) => {
+        const { parentKey: entityKey, fieldName } = info;
+        const allFields = cache.inspectFields(entityKey);
+        const fieldInfos = allFields.filter(
+            (info) => info.fieldName === fieldName
+        );
+        const size = fieldInfos.length;
+        if (size === 0) {
+            return undefined;
+        }
+
+        const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+        const isItInTheCache = cache.resolve(
+            cache.resolve(entityKey, fieldKey) as string,
+            "tests"
+        );
+        info.partial = !isItInTheCache;
+        let hasMore = true;
+        const results: string[] = [];
+        fieldInfos.forEach((fi) => {
+            const key = cache.resolve(entityKey, fi.fieldKey) as string;
+            const data = cache.resolve(key, "tests") as string[];
+            const _hasMore = cache.resolve(key, "hasMore");
+            if (!_hasMore) {
+                hasMore = _hasMore as boolean;
+            }
+            results.push(...data);
+        });
+
+        return {
+            __typename: "PaginatedTests",
+            hasMore,
+            tests: results,
+        };
+    };
+};
 
 export const createUrqlClient = (ssrExchange: any) => ({
     url: "http://localhost:4000/graphql",
-    exchanges: [ssrExchange, dedupExchange, fetchExchange],
+    exchanges: [
+        ssrExchange,
+        dedupExchange,
+        fetchExchange,
+        cacheExchange({
+            keys: {
+                PaginatedTests: () => null,
+            },
+            resolvers: {
+                Query: {
+                    tests: cursorPagination(),
+                },
+            },
+        }),
+    ],
 });
