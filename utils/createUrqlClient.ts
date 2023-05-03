632d52d2a4c4974314acb42fefcb92146a0c5234
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-05-03 21:46:22 +0530

    Invalidate queries

--- utils/createUrqlClient.ts
+++ utils/createUrqlClient.ts
@@ -1,6 +1,14 @@
-import { cacheExchange } from "@urql/exchange-graphcache";
+import { cacheExchange, Cache } from "@urql/exchange-graphcache";
 import { dedupExchange, fetchExchange } from "urql";
 import { relayPagination } from "@urql/exchange-graphcache/extras";
+
+function invalidateQuery(cache: Cache, field: string) {
+    const allFields = cache.inspectFields("Query");
+    const fieldInfos = allFields.filter((info) => info.fieldName === field);
+    fieldInfos.forEach((fi) => {
+        cache.invalidate("Query", field, fi.arguments || {});
+    });
+}
 
 export const createUrqlClient = (ssrExchange: any) => ({
     url: "http://localhost:4000/graphql",
@@ -12,6 +20,20 @@ export const createUrqlClient = (ssrExchange: any) => ({
             resolvers: {
                 Query: {
                     paginatedTests: relayPagination(),
+                },
+            },
+            updates: {
+                Mutation: {
+                    createTest: (_result, args, cache, info) => {
+                        invalidateQuery(cache, "tests");
+                        invalidateQuery(cache, "paginatedTests");
+                        invalidateQuery(cache, "leaderboard");
+                    },
+                    login: (_result, args, cache, info) => {
+                        invalidateQuery(cache, "tests");
+                        invalidateQuery(cache, "paginatedTests");
+                        invalidateQuery(cache, "leaderboard");
+                    },
                 },
             },
         }),
