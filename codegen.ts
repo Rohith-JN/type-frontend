3d36c80733bd40fb6f4beae2fa7f4355cec5e463
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-05-25 12:31:37 +0530

    Codegen backend-URL

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
     overwrite: true,
-    schema: "http://localhost:4000/graphql",
+    schema: process.env.NEXT_PUBLIC_BACKEND_URL,
     documents: "graphql/**/*.graphql",
     generates: {
         "generated/": {
