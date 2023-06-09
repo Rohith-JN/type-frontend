25d5bec56d49d32b94e27a8e01e0dac62577f122
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-06-09 20:50:46 +0530

    Added footer credit

--- components/other/Layout.tsx
+++ components/other/Layout.tsx
@@ -1,12 +1,18 @@
+import Link from "next/link";
 import Navbar from "./Navbar";
 import React, { ReactNode } from 'react';
 
 const Layout = ({ children }: { children: ReactNode }) => {
 
     return (
-        <div>
+        <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
             <Navbar />
-            <main>{children}</main>
+            <main style={{ flex: 1 }}>{children}</main>
+            <div style={{ textAlign: "center", marginTop: "auto", }}>
+                <p style={{ color: "var(--sub-color)", fontFamily: 'lexend', fontWeight: 'normal' }}>
+                    Designed and built by <Link href={"https://github.com/Rohith-JN"} style={{ textDecoration: "underline", color: "var(--sub-color)" }}>Rohith JN</Link>
+                </p>
+            </div>
         </div>
     )
 }
