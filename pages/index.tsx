625fa458a431d2d21a47bbdecd2dd46da1b76480
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2024-09-10 23:35:39 +0530

    Unused variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
 import Header from '../components/index/Header';
 import Test from '../components/index/Test';
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { State } from "../context/state";
-import { setTimerId } from "../context/actions";
-import { recordTest } from "../utils/test";
 import Footer from '../components/index/Footer';
 import ConditionalRenderer from '../components/other/ConditionalRenderer';
 
 const Home = () => {
-  const dispatch = useDispatch();
 
   return <ConditionalRenderer data={true} fetching={false} title={'Type'}>
     <Header />
